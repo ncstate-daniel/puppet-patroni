ncstate-daniel/puppet-patroni

commit 5fd6ae7c24da736006d9cb067d0a3601b8dd2382

Added support to merge pgsql_parameters in hiera
This commit adds the support to merge the pgsql_parameters defined
in different levels of hiera. To merge the parameters, user should
set the value of hiera_merge_pgsql_parameters to 'true'.

manifests/init.pp
@@ -51,6 +51,7 @@ class patroni (
   Array[String] $pgsql_pg_hba          = [],
   Integer $pgsql_pg_ctl_timeout        = $patroni::params::pgsql_pg_ctl_timeout,
   Boolean $pgsql_use_pg_rewind         = $patroni::params::pgsql_use_pg_rewind,
+  Boolean $hiera_merge_pgsql_parameters = $patroni::params::hiera_merge_pgsql_parameters,
   Boolean $pgsql_remove_data_directory_on_rewind_failure = $patroni::params::pgsql_remove_data_directory_on_rewind_failure,
   Array[Hash] $pgsql_replica_method    = [],
 
@@ -129,6 +130,19 @@ class patroni (
   Boolean $restart_service = $patroni::params::restart_service,
 
 ) inherits patroni::params {
+
+  if $hiera_merge_pgsql_parameters == true {
+    $pgsql_parameters_all = lookup( { 'name'          => 'patroni::pgsql_parameters',
+                                      'value_type'    => undef,
+                                      'merge'         => {
+                                         'strategy'   => 'deep',
+                                       }, 
+                                      'default_value' => $pgsql_parameters,
+                                     })
+  } else {
+    $pgsql_parameters_all = $pgsql_parameters
+  }
+
   anchor{'patroni::begin':}
   -> class{'::patroni::install':}
   -> class{'::patroni::config':}

manifests/params.pp
@@ -39,6 +39,7 @@ class patroni::params {
 
   $pgsql_pg_ctl_timeout         = 60
   $pgsql_use_pg_rewind          = true
+  $hiera_merge_pgsql_parameters = false
   $pgsql_remove_data_directory_on_rewind_failure = false
 
   $use_consul    = false

templates/postgresql.yml.erb
@@ -119,9 +119,9 @@ postgresql:
     <%= name -%>: <%= val %>
 <% end -%>
 <% end -%>
-<% unless @pgsql_parameters.empty? -%>
+<% unless @pgsql_parameters_all.empty? -%>
   parameters:
-<% @pgsql_parameters.each do |name,val| -%>
+<% @pgsql_parameters_all.each do |name,val| -%>
     <%= name -%>: <%= val %>
 <% end -%>
 <% end -%>

commit 11ed128cf6bb921d1106dd04367c259abc27ed7d

Fixed the puppet lint issues introduced in previous commit (5fd6ae7c24)

manifests/init.pp
@@ -135,10 +135,10 @@ class patroni (
     $pgsql_parameters_all = lookup( { 'name'          => 'patroni::pgsql_parameters',
                                       'value_type'    => undef,
                                       'merge'         => {
-                                         'strategy'   => 'deep',
-                                       }, 
+                                        'strategy'   => 'deep',
+                                      },
                                       'default_value' => $pgsql_parameters,
-                                     })
+                                    })
   } else {
     $pgsql_parameters_all = $pgsql_parameters
   }